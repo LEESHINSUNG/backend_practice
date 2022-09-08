72681fdcd64a53dc9d15e459c9c96bd13166c0dc
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2022-09-08 23:41:39 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const app = express();
 
-
 // 라우팅
 const home = require("./src/routes/home");
 
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
+console.log("path", `${__dirname}/src/public`);
 app.use("/", home);
 
 module.exports = app;
